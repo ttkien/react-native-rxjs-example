cdfa50804c97397c92244772109eb342175ca005
Kien Tran <kien.tran@employmenthero.com>
2020-02-26 00:57:42 +0700

    "add skip"

--- app/loginScreen/index.js
+++ app/loginScreen/index.js
@@ -1,23 +1,27 @@
-import {Observable, Subject, combineLatest} from 'rxjs';
+import {Observable, Subject, combineLatest, merge} from 'rxjs';
 import React, {useEffect} from 'react';
 import * as RxOperations from 'rxjs/operators';
 
-import login$ from './login$';
 import RxButton from './RxButton';
 import RxText from './RxText';
 import RxTextInput from './RxTextInput';
+import login$ from './login$';
 
 const LoginForm = ({}: {}) => {
+  let errorMessage = new Subject();
   let userName$: Observable<string> = null;
   let password$: Observable<string> = null;
-  let errorMessage = new Subject();
+
   let onPressObsevable: Observable<{}> = null;
   let disable$ = new Subject();
 
   useEffect(() => {
     combineLatest(userName$, password$)
       .pipe(
-        RxOperations.throttleTime(100),
+        // //1
+        // RxOperations.skip(1),
+        // // 2
+        // RxOperations.throttleTime(100),
         RxOperations.map(
           ([username, passsword]): string => {
             return username.length >= 6 && passsword.length >= 6
@@ -33,15 +37,27 @@ const LoginForm = ({}: {}) => {
 
     onPressObsevable
       .pipe(
-        RxOperations.throttleTime(2000),
+        // //3
+        // RxOperations.throttleTime(2000),
+        // // 4.1
+        // RxOperations.tap(() => {
+        //   errorMessage.next('loading text');
+        //   disable$.next(true);
+        // }),
         RxOperations.withLatestFrom(combineLatest(userName$, password$)),
         RxOperations.flatMap(([onPress, userName, passsword]) => {
           return login$(userName, passsword);
         }),
+        // 4.2
+        // RxOperations.tap(() => {
+        //   errorMessage.next('');
+        //   disable$.next(false);
+        // }),
       )
-      .subscribe(() => {
-        alert('login');
+      .subscribe(result => {
+        alert('login stauts ', result.isSuccess);
       });
+
     return () => {};
   });
   return (
